ad409569153a0e8a45ca8cb2b3a7787abdf13ae1
kshiprant <kshiprant19@gmail.com>
2026-03-03 14:28:17 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-# Database credentials
-
-DB_HOST = "YOUR_DB_HOST"
-DB_NAME = "YOUR_DB_NAME"
-DB_USER = "YOUR_DB_USER"
-DB_PASSWORD = "YOUR_DB_PASSWORD"
-DB_PORT = 5432  # default PostgreSQL port
